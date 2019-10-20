0d601677d9bf0bdfee4c3477621d2b60c425cb81
Jonas Linde <jonasjlinde@gmail.com>
2019-10-20 18:54:14 +0200

    switch to firefox fo pdf creation

--- s52pdf.sh
+++ s52pdf.sh
@@ -14,7 +14,7 @@ export GOOGLE_API_KEY=0 GOOGLE_DEFAULT_CLIENT_ID=0 GOOGLE_DEFAULT_CLIENT_SECRET=
 export DISPLAY=:17
 Xvfb $DISPLAY -screen 0 1920x1080x24+32 >/dev/null 2>&1 &
 metacity 2>/dev/null &
-google-chrome --kiosk $name.html 2>/dev/null &
+firefox -no-remote -P other -safe-mode $name.html 2>/dev/null &
 
 i=-6
 while test $i -lt 0
@@ -22,26 +22,31 @@ do
     echo -ne "$i\r"
     sleep 1
     ((i++))
+    case $i in
+        -3) xsetroot -solid green
+            wmctrl -ar firefox
+            xdotool key Return
+            ;;
+        -2) xdotool key F11
+            xdotool mousemove 30 30
+            ;;
+    esac
 done
 echo -ne "  \r"
-
-xsetroot -solid green
-wmctrl -ar google-chrome
-xdotool key F11
-xdotool mousemove 30 30
 
 rm -f capture??.png
 while test $i -lt $n
 do
     echo -ne "$i\r"
-    perl -MTime::HiRes -e 'Time::HiRes::usleep(0.25*1e6)'
-    scrot  $(printf "capture%02d.png" $i)
+    sleep 0.25
+    scrot $(printf "capture%02d.png" $i)
     xdotool click 1
     ((i++))
 done
 echo -ne ".  \r"
 
-xdotool key ctrl+shift+q
+xdotool key ctrl+w
+xdotool key ctrl+w
 
 convert capture??.png $name.pdf
 rm -f capture??.png
