55fe1f1c8547862dc08e731e4dabc4ee34f78ecf
Jonas Linde <jonasjlinde@gmail.com>
2019-11-16 13:00:37 +0100

    speling

--- index.sh
+++ index.sh
@@ -6,7 +6,7 @@ cat <<EOF
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
   <head>
-    <title>Presenttioner</title>
+    <title>Presentationer</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta  name="author" content="Jonas J Linde">
     <link rel="stylesheet" href="b3.css" type="text/css" />
