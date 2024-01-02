f076940c841f24cbc5ddc80b111fedf417be9ccc
Jonas Linde <jonasjlinde@gmail.com>
2024-01-02 17:43:38 +0100

    handle touch screen swipes

--- ui/default/slides.js
+++ ui/default/slides.js
@@ -815,3 +815,35 @@ function b3circles() {
     circle.setAttribute("stroke", color);
   }
 }
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    go(xDiff/Math.abs(xDiff));
+  } else if (yDiff != 0) {
+    go(yDiff/Math.abs(yDiff));
+  }
+
+  xDown = null;
+  yDown = null;
+};
